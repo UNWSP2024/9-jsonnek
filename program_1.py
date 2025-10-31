7eb4ed7c912516dca64f4e4b6c68543fdd6c5166
jsonnek <jtsonnek@students.unwsp.edu>
2025-10-30 19:21:01 -0500

    Wrote programs 1,2, and 3.

--- program_1.py
+++ program_1.py
@@ -1,13 +1,20 @@
+# Jonathan Sonnek
+# October 30 2025
 # Program #1: Item Counter
+
 # Assume a file containing a series of names (as strings) is named names.txt 
 # (Use the included example file names.txt) and exists on the computer's disk.
 # Write a program that displays the number of names that are stored in the file.
 
 def count_file_lines():
     ######################
-    # Add your code here #
+    total_names = 0
+    names = open("names.txt", "r")
+    for line in names:
+      total_names += 1
+    names.close()
     ######################
-    print('In the count_file_lines function')
+    print('In the count_file_lines function there are ' +  str(total_names) + ' names total')
 
 
 
--- program_3.py
+++ program_3.py
@@ -1,4 +1,7 @@
-# Program #3: Average Numbers
+# Jonathan Sonnek
+# October 30 2025
+# Program #3: Total Numbers
+
 # Assume a file containing a series of integers is named numbers.txt and exists on the computer's disk.
 # (please use the provided numbers.txt)
 # Write a program that reads all of the numbers stored in the file and calculates their total.  
@@ -8,11 +11,27 @@
 # It should handle any IOError exceptions that are raised.
 # It should handle any ValueError exceptions that are raised when the items that are read from the file 
 # are converted to a number.
+
 def sum_numbers_from_file():
     ######################
-    # Add your code here #
-    ######################
-    print('In the sum_numbers_from_file function')
+    # Open the numbers.txt file
+    infile = open('numbers.txt', 'r')
+    sum = 0
+    try:
+        # read the values from the file and add them
+        for line in infile:
+            amount = int(line)
+            sum += amount
+    except IOError:
+        print("An error occurred trying to read this file.")
+    except ValueError:
+        print("Non-Numeric Data Found in this file")
+    except:
+        print("An Error Occurred")
+    finally:
+        infile.close()
+        ######################
+    print('The total of the numbers in the file was,' + str(sum))
 
 # You don't need to change anything below this line:
 if __name__ == '__main__':
